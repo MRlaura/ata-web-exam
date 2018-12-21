cbbd791bd67d78247de301024e43d1ccbb4e0663
MRlaura <lauramr85@yahoo.com>
2018-12-21 11:41:43 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+
+var likeCount = 0;
+
+function like(){
+  likeCount++;
+  var espacio = document.getElementById("countLike");
+  espacio.innerHTML= likeCount;
+  
+}
+
+var likeLove = 0;
+
+function love(){
+  likeLove++;
+  var espacio = document.getElementById("countLove");
+  espacio.innerHTML= likeLove;
+  
+}
+
+var likeCoun = 0;
+
+function lik(){
+  likeCoun++;
+  var espacio = document.getElementById("countLik");
+  espacio.innerHTML= likeCoun;
+  
+}
+
+var likeLov = 0;
+
+function lov(){
+  likeLov++;
+  var espacio = document.getElementById("countLov");
+  espacio.innerHTML= likeLov;
+  
+}
+
+
